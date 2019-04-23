32649aee7796be274e00dfde351101c577328588
Ansal S <ansal@bssatech.com>
2019-04-23 16:39:20 +0530

    added script to import stock price

---
+++ stock-update.js
@@ -0,0 +1,54 @@
+'use strict'
+
+// npm packages
+var mongodb = require("mongodb");
+var fs = require("fs");
+
+var mongoClient = new mongodb.MongoClient('mongodb://localhost:27017/marketdata', {useNewUrlParser: true});
+mongoClient.connect(function(err) {
+    if(err) {
+        return console.log("Error connecting to MongoDB");
+    }
+    
+    console.log("DB connection established");
+    var DB = mongoClient.db("marketdata");
+    
+    fs.readFile("data.csv", "utf-8", function(error, fileData) {
+        if(error) { return console.log("error reading file"); }
+
+        fileData = fileData.split("\n");
+
+        var sharePrices = [];
+
+        for(var i = 1; i < fileData.length - 1; i++) {
+            var stockData = fileData[i].split(",");
+            var name = stockData[0];
+            var prevClose = stockData[3];
+            var currentPrice = stockData[8];
+
+            sharePrices.push({
+                name: name,
+                prevClose: parseFloat(prevClose),
+                currentPrice: parseFloat(currentPrice)
+            });
+
+        }
+
+        DB.collection("bhavdata").deleteMany({}, function(error){
+            if(error) { return console.log("error deleting data"); }
+
+            console.log("deleted data.. now inserting...")
+
+            DB.collection("bhavdata").insertMany(sharePrices, function(error){
+                if(error) {
+                    console.log("error inserting data to DB");
+                } else {
+                    console.log("data inserted succesfully");
+                }
+            });
+
+        });
+
+    });
+
+});
